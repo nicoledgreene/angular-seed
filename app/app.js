70e3e625f7363f1acfdd3201eb54a375d17601cc
Nicole Greene <nicole_greene@apple.com>
2020-11-27 13:25:53 -0800

    blog examples

--- app/app.js
+++ app/app.js
@@ -1,6 +1,19 @@
 var myApp = angular.module("myApp", []);
 
-myApp.controller("about", ["$scope", function($scope) {
-  $scope.name = "Joe";
-  $scope.age = 25;
-}]);
+// myApp.controller("about", ["$scope", function($scope) {
+//   $scope.name = "Joe";
+//   $scope.age = 25;
+// }]);
+
+
+myApp.controller("myController", ["$scope", function($scope) {
+  $scope.error = {
+      message: "A server error occurred",
+      code: "500"
+  };   
+}])
+.directive("errorWindow", function() {
+  return {
+      templateUrl: "error-window.html"
+  }
+});
